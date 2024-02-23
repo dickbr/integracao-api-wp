41c206c080d741325d752312a11d7e008a49a090
Jean Pierre <pgdick@gmail.com>
2024-02-23 01:10:36 -0300

    fix-resolve-function

--- integracao-api.php
+++ integracao-api.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(10000);
 /**
  * Plugin Name: Integracao API
  * Plugin URI: https://meusite.com/meu-plugin-woocommerce
@@ -8,48 +9,77 @@
  * License: GPL2
  */
 function integracao_api_init() {
-    $url = 'https://mcstaging.vendas.agis.com.br/rest/all/V1/agis/reseller/product/list';
-    $token = 'lowuhjnbyyy6mwabi4yedt6udc1pgzvd';
-
-    $searchCriteria = array(
-        'currentPage' =>  1,
-        'pageSize' =>  500
-    );
-
-    $url .= '?searchCriteria[currentPage]=' . $searchCriteria['currentPage'];
-    $url .= '&searchCriteria[pageSize]=' . $searchCriteria['pageSize'];
-
-    $produtos_filtrados = integracao_api_filtrar_produtos($url, $token);
-    integracao_api_enviar_produtos($produtos_filtrados);
+    integracao_api_buscar_produtos();
 
     if (!wp_next_scheduled('integracao_api_cron_hook')) {
       wp_schedule_event(time(), 'daily', 'integracao_api_cron_hook');
     } 
 
     add_action('integracao_api_cron_hook', 'integracao_api_init');
-
 }
 
-function integracao_api_filtrar_produtos() {
-    $produtos = integracao_api_buscar_produtos();
+function hasMinPrice($price, $min_price){
+  return $price >= $min_price;
+}
 
-    $produtos_filtrados = array_filter($produtos, function($produto) {
-        return $produto['preco'] >=  500;
-    });
+function getSlug($name){
+  $name = isset($name) ? $name : 'Produto sem nome';
+  return sanitize_title($name);
+}
 
-    $produtos_filtrados = integracao_api_lidar_com_produtos_duplicados($produtos_filtrados);
+function getProductBySku($sku){
+  $args = array(
+    'post_type' => 'product',
+    'meta_query' => array(
+        array(
+            'key' => '_sku',
+            'value' => $sku,
+            'compare' => '=',
+            ),
+        ),
+  );
+  
+  $products = get_posts($args);
 
-    return $produtos_filtrados;
+  if (!empty($products) && isset($products[0])) {
+      return $products[0]->ID;
+  }
+
+  return null;
+}
+
+function updateAndContinue($sku, $price, $product, $currentPage){
+  $post_id = getProductBySku($sku);
+  if(isset($post_id)){
+    $actual_price = get_post_meta($post_id, '_price', true);
+    if($price > $actual_price){
+      error_log('sku: ' . $sku . ' | preço atual: ' . $actual_price . ' | preço api: ' . $price);
+      $product['ID'] = $post_id;
+      $result = wp_update_post($product);
+      logResultQuery($result, $sku, 'atualizado | post_id ' . $post_id, $currentPage);
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+function logResultQuery($result, $sku, $operation, $pag){
+   if (is_wp_error($result)) {
+        error_log('pag '. $pag . ' | Erro ao '. $operation .' o produto com SKU: ' . $sku . ' - ' . $result->get_error_message());
+    } else {
+        error_log('pag '. $pag . ' | Produto '. $operation .' com sucesso: ' . $sku);
+    }
 }
 
 function integracao_api_buscar_produtos() {
     $base_url = 'https://mcstaging.vendas.agis.com.br/rest/all/V1/agis/reseller/product/list';
     $token = 'lowuhjnbyyy6mwabi4yedt6udc1pgzvd';
-    $produtos = array();
+    $products = [];
 
     $currentPage = 1;
-    $pageSize = 500;
-    $totalPages = 1; 
+    $pageSize = 250;
 
     do {
         $searchCriteria = array(
@@ -59,140 +89,89 @@ function integracao_api_buscar_produtos() {
 
         $url = $base_url . '?searchCriteria[currentPage]=' . $searchCriteria['currentPage'] . '&searchCriteria[pageSize]=' . $searchCriteria['pageSize'];
 
-        $ch = curl_init($url);
+        try{
+          $ch = curl_init($url);
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Content-Type: application/json",
-            "Authorization: Bearer $token"
-        ));
-        curl_setopt($ch, CURLOPT_TIMEOUT,  300);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLOPT_TIMEOUT,  5);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              "Content-Type: application/json",
+              "Authorization: Bearer $token"
+          ));
 
-        $response = curl_exec($ch);
-        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+          $response = curl_exec($ch);
 
-        if ($http_code != 200) {
-            break;
-        }
+          curl_close($ch);
 
-        $produtos_pagina = json_decode($response, true);
+          $produtos_pagina = json_decode($response, true);
 
-        if (is_array($produtos_pagina) && isset($produtos_pagina['items'])) {
-            foreach ($produtos_pagina['items'] as &$item) {
-                if (isset($item['stock'][0]['price'])) {
-                    $item['preco'] = $item['stock'][0]['price'];
-                } else {
-                    $item['preco'] = 0;
-                }
+          if(!isset($produtos_pagina)){
+            error_log('nenhum dado retornado da api');
+            $currentPage++;
+            continue;
+          }
+
+          $items = $produtos_pagina['items'];
+
+          if(empty($items)){
+            error_log('produtos não encontrados na api');
+            $products = [];
+            $currentPage++;
+            continue;
+          }
+
+          $products = $produtos_pagina;
+
+          foreach ($produtos_pagina['items'] as &$item) {
+            $price = $item['stock'][0]['price'];
+            $sku = $item['sku'];
+            if(!hasMinPrice($price, 500)){
+              error_log('produto não possui valor minimo de R$500,00 | preço: '. $price .' | sku: ' . $sku);
+              continue;
             }
-            $produtos = array_merge($produtos, $produtos_pagina['items']);
-        }
 
-        $currentPage++;
-
-    } while ($currentPage <= $totalPages);
-
-    return $produtos;
-}
-
-function integracao_api_lidar_com_produtos_duplicados($produtos) {
-    $produtos_unicos = array();
-    $produtos_duplicados = array();
-
-    foreach ($produtos as $produto) {
-        $sku = $produto['sku'];
-        $warehouse = isset($produto['warehouse']) ? $produto['warehouse'] : null;
-
-        $preco = $produto['preco'];
-
-        if (isset($produtos_duplicados[$sku])) {
-            if ($preco < $produtos_duplicados[$sku]['preco'] && $preco >  500) {
-                $produtos_duplicados[$sku] = $produto;
-            }
-        } else {
-            $produtos_duplicados[$sku] = $produto;
-            $produtos_unicos[] = $produto;
-        }
-    }
-
-    return $produtos_unicos;
-}
-
-function integracao_api_enviar_produtos($produtos) {
-    foreach ($produtos as $produto) {
-        $post_id = wc_get_product_id_by_sku($produto['sku']);
-
-        if (!isset($produto['preco'])) {
-            error_log('A chave "preco" não foi encontrada no array do produto com SKU: ' . $produto['sku']);
-            continue;   
-        }
-
-        $nome = isset($produto['name']) ? $produto['name'] : 'Produto sem nome';
-        $slug = sanitize_title($nome);
-
-        if (empty($nome)) {
-            $nome = 'Produto ' . $produto['sku'];
-            $slug = sanitize_title($nome);
-        }
-        $post_data = array(
-            'post_title'    => $nome,
-            'post_name'     => $slug,
-            'post_content'  => '',
-            'post_status'   => 'publish',
-            'post_author'   =>   1,
-            'post_type'     => 'product',
-            'meta_input'    => array(
-                '_price' => $produto['preco'],
-                '_sku'   => $produto['sku'],
+            $slug = getSlug($item['name']);
+            
+            $product = [
+              'post_title' =>  $item['name'],
+              'post_name' =>  $slug,
+              'post_status'   => 'publish',
+              'post_author'   =>   1,
+              'post_type'     => 'product',
+              'meta_input' => [
+                '_sku' => $sku,
+                '_quantity' => $item['stock'][0]['qty'],
+                '_price' => $price,
                 '_visibility' => 'visible',
                 '_stock_status' => 'instock',
                 '_backorders' => 'no',
                 '_sold_individually' => 'no',
-                '_weight' => '',
-                '_length' => '',
-                '_width' => '',
-                '_height' => '',
-                '_upsell_ids' => '',
-                '_crosssell_ids' => '',
-                '_purchase_note' => '',
-                '_default_attributes' => '',
-                '_virtual' => 'no',
-                '_downloadable' => 'no',
-                '_download_limit' => '',
-                '_download_expiry' => '',
-                '_downloadable_files' => '',
-                '_featured' => 'no',
-                '_product_attributes' => '',
-                '_wc_rating_count' => '',
-                '_wc_average_rating' => '',
-                '_wc_review_count' => '',
-                '_variation_description' => '',
-                '_thumbnail_id' => '',
-                '_product_image_gallery' => '',
-                '_sale_price' => '',
-                '_sale_price_dates_from' => '',
-                '_sale_price_dates_to' => '',
-            ),
-        );
-        if ($post_id) {
-            $post_data['ID'] = $post_id;
-            $update_result = wp_update_post($post_data);
-            if (is_wp_error($update_result)) {
-                error_log('Erro ao atualizar o produto com SKU: ' . $produto['sku'] . ' - ' . $update_result->get_error_message());
-            } else {
-                error_log('Produto atualizado com sucesso: ' . $produto['sku']);
+              ]
+            ];
+
+              $continue = updateAndContinue($sku, $price, $product, $currentPage);
+              
+              if($continue) {
+                continue;
+              }
+
+              $result = wp_insert_post($product);
+
+              logResultQuery($result, $sku, 'inserido', $currentPage);
             }
-        } else {
-            $insert_result = wp_insert_post($post_data);
-            if (is_wp_error($insert_result)) {
-                error_log('Erro ao inserir o novo produto com SKU: ' . $produto['sku'] . ' - ' . $insert_result->get_error_message());
-            } else {
-                error_log('Produto criado com sucesso: ' . $produto['sku']);
-            }
+
+          $currentPage++;
+        } catch (Exception $e) {
+           error_log('pag::error' . $currentPage . ' | Erro: ' . $e->getMessage());
+           $currentPage++;
+           continue;
         }
-    }
+
+    // } while ($currentPage <= 1);
+    } while (!empty($products));
+
 }
+
 
 function integracao_api_agendar_cron() {
     if (!wp_next_scheduled('integracao_api_cron_job')) {
